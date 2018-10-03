f87018cda425305dd11305aa87047b2efa354a00
ccx <ci250454344@163.com>
2018-10-03 10:35:44 +0800

    更新

--- liveplayback/src/main/java/com/hejunlin/liveplayback/MainActivity.java
+++ liveplayback/src/main/java/com/hejunlin/liveplayback/MainActivity.java
@@ -1,8 +1,8 @@
 /*
  * Copyright (C) 2016 hejunlin <hejunlin2013@gmail.com>
- * 
+ *
  * Github:https://github.com/hejunlin2013/LivePlayback
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,13 +17,17 @@
  */
 package com.hejunlin.liveplayback;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 import com.hejunlin.liveplayback.widget.MetroViewBorderImpl;
 
@@ -32,7 +36,6 @@ import java.util.Date;
 public class MainActivity extends Activity {
 
     private MetroViewBorderImpl mMetroViewBorderImpl;
-
 
 
     @Override
@@ -44,8 +47,20 @@ public class MainActivity extends Activity {
         loadRecyclerViewMenuItem();
     }
 
+    @SuppressLint("SetTextI18n")
     private void loadRecyclerViewMenuItem() {
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.ry_menu_item);
+        TextView version = (TextView) findViewById(R.id.version);
+        try {
+            PackageInfo packageInfo = this.getApplicationContext()
+                    .getPackageManager()
+                    .getPackageInfo(this.getPackageName(), 0);
+            String versionName = packageInfo.versionName;
+            int   versionCode = packageInfo.versionCode;
+            version.setText("版本 : " + versionName + "." + versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        RecyclerView      recyclerView  = (RecyclerView) findViewById(R.id.ry_menu_item);
         GridLayoutManager layoutManager = new GridLayoutManager(this, 1);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
